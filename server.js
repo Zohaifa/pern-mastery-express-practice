95bea97d47ea5dc5456f2c81eaf004d76b4256a7
Zohaifa <ultimatezrage@gmail.com>
2025-12-06 20:05:52 +0600

    add category methods and data.json

--- server.js
+++ server.js
@@ -5,8 +5,21 @@ app.use(express.json());
 
 app.get("/", (req, res) =>{
     res.send("Home page");
+});
+
+app.get("/categories", (req, res)=>{
+    res.send("category fetched");
+})
+app.post("/categories", (req, res)=>{
+    res.send("category added");
+})
+app.put("/categories", (req, res)=>{
+    res.send("category updated");
+})
+app.delete("/categories", (req, res)=>{
+    res.send("category deleted");
 })
 
 app.listen(3000, ()=>{
     console.log("Listening to port http://localhost:3000");
-})
+});
