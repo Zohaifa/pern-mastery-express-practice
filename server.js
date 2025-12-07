bd01148373b660c0c18e1d4a5ddbf526356bb9ff
Zohaifa <ultimatezrage@gmail.com>
2025-12-07 22:01:15 +0600

    create user Schema using zod library

--- server.js
+++ server.js
@@ -1,55 +1,23 @@
-const express = require('express');
+import express from "express";
+import {z} from "zod";
 const app = express();
-const fs = require('node:fs');
 
 app.use(express.json());
 
-app.get("/", (req, res) =>{
-    res.send("Home page");
-});
-
-app.get("/categories", (req, res)=>{
-    const data = fs.readFileSync('data.json', 'utf-8');
-    const dataObj = JSON.parse(data);
-    res.json(dataObj);
-})
-app.get("/categories/:id", (req, res)=>{
-    const targetId = parseInt(req.params.id);
-    const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-    for(let i = 0; i<data.categories.length; i++){
-        console.log(data.categories[i]);
-        if(data.categories[i].id === targetId){
-            res.json(data.categories[i]);
-        }
+app.get("/auth/sign-up", (req, res)=>{
+    const userData = req.body;
+    const createUserSchema = z.object({
+        firstName : z.string().min(3),
+        lastName : z.string().min(3),
+        email : z.email(),
+        password : z.string().min(5),
+    })
+    const {success, data, error} = createUserSchema.safeParse(userData);
+    if(success){
+        res.send(data);
+    }else{
+        res.send(z.prettifyError(error));
     }
-    res.send("no such products with that id");
-})
-app.post("/categories", (req, res)=>{
-    const data = fs.readFileSync('data.json', 'utf-8');
-    const dataObj = JSON.parse(data);
-    const newData = req.body;
-    dataObj.categories.push(newData);
-    fs.writeFileSync('data.json', JSON.stringify(dataObj, null, 2));
-    res.send("category added");
-})
-app.put("/categories", (req, res)=>{
-    const data = fs.readFileSync('data.json', 'utf-8');
-    let dataObj = JSON.parse(data);
-    for(let i = 0; i < dataObj.categories.length; i++){
-        if(dataObj.categories[i].id === req.body.id){
-            dataObj.categories[i] = req.body;
-        }
-    }
-    fs.writeFileSync("data.json", JSON.stringify(dataObj, null, 2));
-    res.send("category updated");
-})
-app.delete("/categories", (req, res)=>{
-    const data = fs.readFileSync('data.json', 'utf-8');
-    let dataObj = JSON.parse(data);
-    const targetId = parseInt(req.body.id);
-    dataObj.categories = dataObj.categories.filter((element)=> element.id !== targetId);
-    fs.writeFileSync("data.json", JSON.stringify(dataObj, null, 2));
-    res.send("category deleted");
 })
 
 app.listen(3000, ()=>{
