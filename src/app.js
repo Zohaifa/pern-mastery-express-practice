53217af3d6b78ddc3269eeb6fa355edc29e77e77
Zohaifa <ultimatezrage@gmail.com>
2025-12-15 20:41:53 +0600

    create sign in api

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res)=>{
+    res.send("It works man!");
+});
+
 app.post("/auth/sign-up", async (req, res)=>{
     const userCreateSchema = z.object({
         firstName : z.string().min(3),
@@ -18,7 +22,7 @@ app.post("/auth/sign-up", async (req, res)=>{
     const {success, data, error} = userCreateSchema.safeParse(req.body);
 
     if(!success){
-        res.status(400).json({message: "validation failed", data: z.flattenError(error)});
+        res.status(400).json({status: "fail", message: "validation failed", "error": z.flattenError(error)});
     }
     
     const hashedPassword = await bcrypt.hash(data.password, 5);
@@ -34,8 +38,40 @@ app.post("/auth/sign-up", async (req, res)=>{
         data: user
     });
 
-    res.json({user: createdUser});
+    res.status(200).json({user: createdUser});
 });
+
+app.post("/auth/sign-in", async (req, res)=>{
+    const userLoginSchema = z.object({
+        email: z.email(),
+        password: z.string().min(3)
+    })
+    
+    const {success, data, error} = userLoginSchema.safeParse(req.body);
+    
+    if(!success){
+        res.status(400).json({status: "Failure", message: "Validation failed", "error": z.flattenError(error)});
+    }
+
+    const user = await prisma.user.findUnique({
+        where:{
+            email: data.email
+        },
+    })
+    if(!user){
+        res.status(404).json({status: "Failure", message: "User not found"});
+    }
+
+    const passwordValidation = await bcrypt.compare(data.password, user.passwordHash);
+
+    if(!passwordValidation){
+        res.status(401).json({status: "Failure", message: "Email and Password doesn't match"});
+    }
+
+    res.status(200).json({status: "success", message: "User retrieved", data: user});
+});
+
+
 
 app.listen(3000, ()=>{
     console.log("Listening to port http://localhost:3000");
